332b6dd6846de66f943a4bc38359f7abbd8c083a
Nikolay <44751173+nklyy@users.noreply.github.com>
2022-02-09 19:37:52 +0200

    add uber zap logger

--- cmd/main.go
+++ cmd/main.go
@@ -4,14 +4,19 @@ import (
 	"fmt"
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
-	"github.com/sirupsen/logrus"
+	"go.uber.org/zap"
+	"log"
 	"net/http"
 	"nn-realtime-support-chat/internal/health"
 )
 
 func main() {
 	// Init logger
-	logger := logrus.New()
+	logger, err := zap.NewProduction()
+	if err != nil {
+		log.Fatalf("can't initialize zap logger: %v", err)
+	}
+	defer logger.Sync()
 
 	// Set-up Route
 	router := chi.NewRouter()
@@ -25,10 +30,10 @@ func main() {
 	})
 
 	// Start App
-	err := http.ListenAndServe(":5000", router)
+	err = http.ListenAndServe(":5000", router)
 	if err != nil {
 		fmt.Println(err)
-		logger.Fatalln("Failed to start HTTP server!")
+		logger.Fatal("Failed to start HTTP server!", zap.Error(err))
 		return
 	}
 }
