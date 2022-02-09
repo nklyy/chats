2e8cc3b385d063db640196183d79f9ba41b488b2
Nikolay <44751173+nklyy@users.noreply.github.com>
2022-02-09 19:06:41 +0200

    add error codes add respond helper add chi add health handler add test for health handler

---
+++ cmd/main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+	"github.com/sirupsen/logrus"
+	"net/http"
+	"nn-realtime-support-chat/health"
+)
+
+func main() {
+	// Init logger
+	logger := logrus.New()
+
+	// Set-up Route
+	router := chi.NewRouter()
+	router.Use(middleware.Logger)
+
+	// Handlers
+	healthHandler := health.NewHandler()
+
+	router.Route("/api/v1", func(r chi.Router) {
+		healthHandler.SetupRoutes(r)
+	})
+
+	// Start App
+	err := http.ListenAndServe(":5000", router)
+	if err != nil {
+		fmt.Println(err)
+		logger.Fatalln("Failed to start HTTP server!")
+		return
+	}
+}
