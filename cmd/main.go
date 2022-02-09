3ab5d677dab9c92ccbbbcb6ef345b1218062469e
Nikolay <44751173+nklyy@users.noreply.github.com>
2022-02-09 19:08:06 +0200

    refactoring

--- cmd/main.go
+++ cmd/main.go
@@ -6,7 +6,7 @@ import (
 	"github.com/go-chi/chi/v5/middleware"
 	"github.com/sirupsen/logrus"
 	"net/http"
-	"nn-realtime-support-chat/health"
+	"nn-realtime-support-chat/internal/health"
 )
 
 func main() {
