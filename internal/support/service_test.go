9d045938cbbc36e55b1fd33cdf1fa77b0b818645
Nikolay <44751173+nklyy@users.noreply.github.com>
2022-03-17 13:44:25 +0200

    update tests

--- internal/support/service_test.go
+++ internal/support/service_test.go
@@ -2,13 +2,10 @@ package support_test
 
 import (
 	"context"
-	gjwt "github.com/golang-jwt/jwt"
 	"github.com/golang/mock/gomock"
 	"github.com/stretchr/testify/assert"
 	"go.uber.org/zap"
 	"noname-realtime-support-chat/internal/support"
-	"noname-realtime-support-chat/internal/support/jwt"
-	"noname-realtime-support-chat/internal/support/jwt/mocks"
 	mock_support "noname-realtime-support-chat/internal/support/mocks"
 	"noname-realtime-support-chat/pkg/logger"
 	"testing"
@@ -25,7 +22,6 @@ func TestNewService(t *testing.T) {
 		repository support.Repository
 		logger     *zap.SugaredLogger
 		salt       *int
-		jwtSvc     jwt.Service
 		expect     func(*testing.T, support.Service, error)
 	}{
 		{
@@ -33,7 +29,6 @@ func TestNewService(t *testing.T) {
 			repository: mock_support.NewMockRepository(controller),
 			logger:     &zap.SugaredLogger{},
 			salt:       &salt,
-			jwtSvc:     mock_jwt.NewMockService(controller),
 			expect: func(t *testing.T, s support.Service, err error) {
 				assert.NotNil(t, s)
 				assert.Nil(t, err)
@@ -44,7 +39,6 @@ func TestNewService(t *testing.T) {
 			repository: nil,
 			logger:     &zap.SugaredLogger{},
 			salt:       &salt,
-			jwtSvc:     mock_jwt.NewMockService(controller),
 			expect: func(t *testing.T, s support.Service, err error) {
 				assert.Nil(t, s)
 				assert.NotNil(t, err)
@@ -56,7 +50,6 @@ func TestNewService(t *testing.T) {
 			repository: mock_support.NewMockRepository(controller),
 			logger:     nil,
 			salt:       &salt,
-			jwtSvc:     mock_jwt.NewMockService(controller),
 			expect: func(t *testing.T, s support.Service, err error) {
 				assert.Nil(t, s)
 				assert.NotNil(t, err)
@@ -68,30 +61,17 @@ func TestNewService(t *testing.T) {
 			repository: mock_support.NewMockRepository(controller),
 			logger:     &zap.SugaredLogger{},
 			salt:       nil,
-			jwtSvc:     mock_jwt.NewMockService(controller),
 			expect: func(t *testing.T, s support.Service, err error) {
 				assert.Nil(t, s)
 				assert.NotNil(t, err)
 				assert.EqualError(t, err, "invalid salt")
 			},
 		},
-		{
-			name:       "should return invalid jwt service",
-			repository: mock_support.NewMockRepository(controller),
-			logger:     &zap.SugaredLogger{},
-			salt:       &salt,
-			jwtSvc:     nil,
-			expect: func(t *testing.T, s support.Service, err error) {
-				assert.Nil(t, s)
-				assert.NotNil(t, err)
-				assert.EqualError(t, err, "invalid jwt service")
-			},
-		},
 	}
 
 	for _, tc := range tests {
 		t.Run(tc.name, func(t *testing.T) {
-			svc, err := support.NewService(tc.repository, tc.logger, tc.salt, tc.jwtSvc)
+			svc, err := support.NewService(tc.repository, tc.logger, tc.salt)
 			tc.expect(t, svc, err)
 		})
 	}
@@ -102,14 +82,12 @@ func TestService_GetSupportById(t *testing.T) {
 	defer controller.Finish()
 
 	mockRepo := mock_support.NewMockRepository(controller)
-	mockJwt := mock_jwt.NewMockService(controller)
-
 	salt := 10
 
 	newLogger, _ := logger.NewLogger("development")
 	zapLogger, _ := newLogger.SetupZapLogger()
 
-	service, _ := support.NewService(mockRepo, zapLogger, &salt, mockJwt)
+	service, _ := support.NewService(mockRepo, zapLogger, &salt)
 
 	supportEntity, _ := support.NewSupport("email", "name", "password", &salt)
 	supportDTO := support.MapToDTO(supportEntity)
@@ -157,60 +135,116 @@ func TestService_GetSupportById(t *testing.T) {
 	}
 }
 
-func TestService_Registration(t *testing.T) {
+func TestService_GetSupportByEmail(t *testing.T) {
 	controller := gomock.NewController(t)
 	defer controller.Finish()
 
 	mockRepo := mock_support.NewMockRepository(controller)
-	mockJwt := mock_jwt.NewMockService(controller)
-
 	salt := 10
 
 	newLogger, _ := logger.NewLogger("development")
 	zapLogger, _ := newLogger.SetupZapLogger()
 
-	service, _ := support.NewService(mockRepo, zapLogger, &salt, mockJwt)
+	service, _ := support.NewService(mockRepo, zapLogger, &salt)
 
 	supportEntity, _ := support.NewSupport("email", "name", "password", &salt)
-	var emptyStr string
+	supportDTO := support.MapToDTO(supportEntity)
 
 	tests := []struct {
 		name   string
 		ctx    context.Context
-		dto    *support.RegistrationDTO
-		setup  func(context.Context, *support.RegistrationDTO)
-		expect func(*testing.T, *string, error)
+		email  string
+		setup  func(context.Context, string)
+		expect func(*testing.T, *support.DTO, error)
 	}{
 		{
-			name: "should return registered support id",
-			ctx:  context.Background(),
-			dto: &support.RegistrationDTO{
-				Email:    "email",
-				Name:     "name",
-				Password: "password",
+			name:  "should return support",
+			ctx:   context.Background(),
+			email: "email",
+			setup: func(ctx context.Context, email string) {
+				mockRepo.EXPECT().GetSupportByEmail(ctx, email).Return(supportEntity, nil)
 			},
-			setup: func(ctx context.Context, dto *support.RegistrationDTO) {
-				mockRepo.EXPECT().CreateSupport(ctx, gomock.Any()).Return(supportEntity.ID.Hex(), nil)
-			},
-			expect: func(t *testing.T, s *string, err error) {
-				assert.NotNil(t, s)
+			expect: func(t *testing.T, dto *support.DTO, err error) {
+				assert.NotNil(t, dto)
 				assert.Nil(t, err)
-				assert.Equal(t, supportEntity.ID.Hex(), *s)
+				assert.Equal(t, dto.Email, supportDTO.Email)
 			},
 		},
 		{
-			name: "should return failed to create support",
-			ctx:  context.Background(),
-			dto: &support.RegistrationDTO{
-				Email:    "email",
-				Name:     "name",
-				Password: "password",
+			name:  "should return not found",
+			ctx:   context.Background(),
+			email: "email",
+			setup: func(ctx context.Context, email string) {
+				mockRepo.EXPECT().GetSupportByEmail(ctx, email).Return(nil, support.ErrNotFound)
 			},
-			setup: func(ctx context.Context, dto *support.RegistrationDTO) {
+			expect: func(t *testing.T, dto *support.DTO, err error) {
+				assert.Nil(t, dto)
+				assert.Equal(t, support.ErrNotFound, err)
+			},
+		},
+	}
+
+	for _, tc := range tests {
+		t.Run(tc.name, func(t *testing.T) {
+			tc.setup(tc.ctx, tc.email)
+			s, err := service.GetSupportByEmail(tc.ctx, tc.email)
+			tc.expect(t, s, err)
+		})
+	}
+}
+
+func TestService_CreateSupport(t *testing.T) {
+	controller := gomock.NewController(t)
+	defer controller.Finish()
+
+	mockRepo := mock_support.NewMockRepository(controller)
+	salt := 10
+
+	newLogger, _ := logger.NewLogger("development")
+	zapLogger, _ := newLogger.SetupZapLogger()
+
+	service, _ := support.NewService(mockRepo, zapLogger, &salt)
+
+	supportEntity, _ := support.NewSupport("email", "name", "password", &salt)
+	supportDTO := support.MapToDTO(supportEntity)
+
+	var emptyStr string
+
+	tests := []struct {
+		testName string
+		ctx      context.Context
+		email    string
+		name     string
+		password string
+		setup    func(context.Context, string, string, string)
+		expect   func(*testing.T, *support.DTO, error)
+	}{
+		{
+			testName: "should return support",
+			ctx:      context.Background(),
+			email:    "email",
+			name:     "name",
+			password: "password",
+			setup: func(ctx context.Context, email, name, password string) {
+				mockRepo.EXPECT().CreateSupport(ctx, gomock.Any()).Return(supportEntity.ID.Hex(), nil)
+			},
+			expect: func(t *testing.T, dto *support.DTO, err error) {
+				assert.NotNil(t, dto)
+				assert.Nil(t, err)
+				assert.Equal(t, dto.Email, supportDTO.Email)
+			},
+		},
+		{
+			testName: "should return support",
+			ctx:      context.Background(),
+			email:    "email",
+			name:     "name",
+			password: "password",
+			setup: func(ctx context.Context, email, name, password string) {
 				mockRepo.EXPECT().CreateSupport(ctx, gomock.Any()).Return(emptyStr, support.ErrFailedSaveSupport)
 			},
-			expect: func(t *testing.T, s *string, err error) {
-				assert.Empty(t, s)
+			expect: func(t *testing.T, dto *support.DTO, err error) {
+				assert.Empty(t, dto)
 				assert.NotNil(t, err)
 				assert.EqualError(t, err, support.ErrFailedSaveSupport.Error())
 			},
@@ -219,311 +253,9 @@ func TestService_Registration(t *testing.T) {
 
 	for _, tc := range tests {
 		t.Run(tc.name, func(t *testing.T) {
-			tc.setup(tc.ctx, tc.dto)
-			s, err := service.Registration(tc.ctx, tc.dto)
+			tc.setup(tc.ctx, tc.email, tc.name, tc.password)
+			s, err := service.CreateSupport(tc.ctx, tc.email, tc.name, tc.password)
 			tc.expect(t, s, err)
-		})
-	}
-}
-
-func TestService_Login(t *testing.T) {
-	controller := gomock.NewController(t)
-	defer controller.Finish()
-
-	mockRepo := mock_support.NewMockRepository(controller)
-	mockJwt := mock_jwt.NewMockService(controller)
-
-	salt := 10
-
-	newLogger, _ := logger.NewLogger("development")
-	zapLogger, _ := newLogger.SetupZapLogger()
-
-	service, _ := support.NewService(mockRepo, zapLogger, &salt, mockJwt)
-
-	supportEntity, _ := support.NewSupport("email", "name", "password", &salt)
-	tokenAccess := "tokenAccess"
-	tokenRefresh := "tokenRefresh"
-	var emptyStr string
-
-	tests := []struct {
-		name   string
-		ctx    context.Context
-		dto    *support.LoginDTO
-		setup  func(context.Context, *support.LoginDTO)
-		expect func(*testing.T, *string, *string, error)
-	}{
-		{
-			name: "should return jwt token",
-			ctx:  context.Background(),
-			dto: &support.LoginDTO{
-				Email:    "email",
-				Password: "password",
-			},
-			setup: func(ctx context.Context, dto *support.LoginDTO) {
-				mockRepo.EXPECT().GetSupportByEmail(ctx, dto.Email).Return(supportEntity, nil)
-				mockJwt.EXPECT().CreateTokens(ctx, supportEntity.ID.Hex(), "support").Return(&tokenAccess, &tokenRefresh, nil)
-			},
-			expect: func(t *testing.T, a *string, r *string, err error) {
-				assert.NotNil(t, a)
-				assert.NotNil(t, r)
-				assert.Nil(t, err)
-				assert.Equal(t, *a, tokenAccess)
-				assert.Equal(t, *r, tokenRefresh)
-			},
-		},
-		{
-			name: "should return failed to find support",
-			ctx:  context.Background(),
-			dto: &support.LoginDTO{
-				Email:    "email",
-				Password: "password",
-			},
-			setup: func(ctx context.Context, dto *support.LoginDTO) {
-				mockRepo.EXPECT().GetSupportByEmail(ctx, dto.Email).Return(nil, support.ErrNotFound)
-			},
-			expect: func(t *testing.T, a *string, r *string, err error) {
-				assert.Empty(t, a)
-				assert.Empty(t, r)
-				assert.NotNil(t, err)
-				assert.EqualError(t, err, support.ErrNotFound.Error())
-			},
-		},
-		{
-			name: "should return failed to create jwt token",
-			ctx:  context.Background(),
-			dto: &support.LoginDTO{
-				Email:    "email",
-				Password: "password",
-			},
-			setup: func(ctx context.Context, dto *support.LoginDTO) {
-				mockRepo.EXPECT().GetSupportByEmail(ctx, dto.Email).Return(supportEntity, nil)
-				mockJwt.EXPECT().CreateTokens(ctx, supportEntity.ID.Hex(), "support").Return(&emptyStr, &emptyStr, jwt.ErrFailedCreateTokens)
-			},
-			expect: func(t *testing.T, a *string, r *string, err error) {
-				assert.Empty(t, a)
-				assert.Empty(t, r)
-				assert.NotNil(t, err)
-				assert.EqualError(t, err, jwt.ErrFailedCreateTokens.Error())
-			},
-		},
-	}
-
-	for _, tc := range tests {
-		t.Run(tc.name, func(t *testing.T) {
-			tc.setup(tc.ctx, tc.dto)
-			a, r, err := service.Login(tc.ctx, tc.dto)
-			tc.expect(t, a, r, err)
-		})
-	}
-}
-
-func TestService_Refresh(t *testing.T) {
-	controller := gomock.NewController(t)
-	defer controller.Finish()
-
-	mockRepo := mock_support.NewMockRepository(controller)
-	mockJwt := mock_jwt.NewMockService(controller)
-
-	salt := 10
-
-	newLogger, _ := logger.NewLogger("development")
-	zapLogger, _ := newLogger.SetupZapLogger()
-
-	service, _ := support.NewService(mockRepo, zapLogger, &salt, mockJwt)
-
-	payload := jwt.Payload{
-		Id:             "id",
-		Role:           "role",
-		Uid:            "uid",
-		StandardClaims: gjwt.StandardClaims{},
-	}
-	tokenAccess := "tokenAccess"
-	tokenRefresh := "tokenRefresh"
-	var emptyStr string
-
-	tests := []struct {
-		name   string
-		ctx    context.Context
-		dto    *support.RefreshDTO
-		setup  func(context.Context, *support.RefreshDTO)
-		expect func(*testing.T, *string, *string, error)
-	}{
-		{
-			name: "should return tokens",
-			ctx:  context.Background(),
-			dto: &support.RefreshDTO{
-				Token: "token",
-			},
-			setup: func(ctx context.Context, dto *support.RefreshDTO) {
-				mockJwt.EXPECT().ParseToken(dto.Token, false).Return(&payload, nil)
-				mockJwt.EXPECT().VerifyToken(ctx, &payload, false).Return(nil)
-				mockJwt.EXPECT().CreateTokens(ctx, payload.Id, "support").Return(&tokenAccess, &tokenRefresh, nil)
-			},
-			expect: func(t *testing.T, a *string, r *string, err error) {
-				assert.NotNil(t, a)
-				assert.NotNil(t, r)
-				assert.Nil(t, err)
-				assert.Equal(t, *a, tokenAccess)
-				assert.Equal(t, *r, tokenRefresh)
-			},
-		},
-		{
-			name: "should return failed parse token",
-			ctx:  context.Background(),
-			dto: &support.RefreshDTO{
-				Token: "token",
-			},
-			setup: func(ctx context.Context, dto *support.RefreshDTO) {
-				mockJwt.EXPECT().ParseToken(dto.Token, false).Return(nil, jwt.ErrToken)
-			},
-			expect: func(t *testing.T, a *string, r *string, err error) {
-				assert.Empty(t, a)
-				assert.Empty(t, r)
-				assert.NotNil(t, err)
-				assert.EqualError(t, err, jwt.ErrToken.Error())
-			},
-		},
-		{
-			name: "should return failed to verify token",
-			ctx:  context.Background(),
-			dto: &support.RefreshDTO{
-				Token: "token",
-			},
-			setup: func(ctx context.Context, dto *support.RefreshDTO) {
-				mockJwt.EXPECT().ParseToken(dto.Token, false).Return(&payload, nil)
-				mockJwt.EXPECT().VerifyToken(ctx, &payload, false).Return(jwt.ErrNotFound)
-			},
-			expect: func(t *testing.T, a *string, r *string, err error) {
-				assert.Empty(t, a)
-				assert.Empty(t, r)
-				assert.NotNil(t, err)
-				assert.EqualError(t, err, jwt.ErrNotFound.Error())
-			},
-		},
-		{
-			name: "should return failed to create jwt token",
-			ctx:  context.Background(),
-			dto: &support.RefreshDTO{
-				Token: "token",
-			},
-			setup: func(ctx context.Context, dto *support.RefreshDTO) {
-				mockJwt.EXPECT().ParseToken(dto.Token, false).Return(&payload, nil)
-				mockJwt.EXPECT().VerifyToken(ctx, &payload, false).Return(nil)
-				mockJwt.EXPECT().CreateTokens(ctx, payload.Id, "support").Return(&emptyStr, &emptyStr, jwt.ErrFailedCreateTokens)
-			},
-			expect: func(t *testing.T, a *string, r *string, err error) {
-				assert.Empty(t, a)
-				assert.Empty(t, r)
-				assert.NotNil(t, err)
-				assert.EqualError(t, err, jwt.ErrFailedCreateTokens.Error())
-			},
-		},
-	}
-
-	for _, tc := range tests {
-		t.Run(tc.name, func(t *testing.T) {
-			tc.setup(tc.ctx, tc.dto)
-			a, r, err := service.Refresh(tc.ctx, tc.dto)
-			tc.expect(t, a, r, err)
-		})
-	}
-}
-
-func TestService_Logout(t *testing.T) {
-	controller := gomock.NewController(t)
-	defer controller.Finish()
-
-	mockRepo := mock_support.NewMockRepository(controller)
-	mockJwt := mock_jwt.NewMockService(controller)
-
-	salt := 10
-
-	newLogger, _ := logger.NewLogger("development")
-	zapLogger, _ := newLogger.SetupZapLogger()
-
-	service, _ := support.NewService(mockRepo, zapLogger, &salt, mockJwt)
-
-	payload := jwt.Payload{
-		Id:             "id",
-		Role:           "role",
-		Uid:            "uid",
-		StandardClaims: gjwt.StandardClaims{},
-	}
-
-	tests := []struct {
-		name   string
-		ctx    context.Context
-		dto    *support.LogoutDTO
-		setup  func(context.Context, *support.LogoutDTO)
-		expect func(*testing.T, error)
-	}{
-		{
-			name: "should logout support",
-			ctx:  context.Background(),
-			dto: &support.LogoutDTO{
-				Token: "token",
-			},
-			setup: func(ctx context.Context, dto *support.LogoutDTO) {
-				mockJwt.EXPECT().ParseToken(dto.Token, true).Return(&payload, nil)
-				mockJwt.EXPECT().VerifyToken(ctx, &payload, true).Return(nil)
-				mockJwt.EXPECT().DeleteTokens(ctx, &payload).Return(nil)
-			},
-			expect: func(t *testing.T, err error) {
-				assert.Nil(t, err)
-			},
-		},
-		{
-			name: "should failed parse token",
-			ctx:  context.Background(),
-			dto: &support.LogoutDTO{
-				Token: "token",
-			},
-			setup: func(ctx context.Context, dto *support.LogoutDTO) {
-				mockJwt.EXPECT().ParseToken(dto.Token, true).Return(nil, jwt.ErrToken)
-			},
-			expect: func(t *testing.T, err error) {
-				assert.NotNil(t, err)
-				assert.EqualError(t, err, jwt.ErrToken.Error())
-			},
-		},
-		{
-			name: "should failed to verify token",
-			ctx:  context.Background(),
-			dto: &support.LogoutDTO{
-				Token: "token",
-			},
-			setup: func(ctx context.Context, dto *support.LogoutDTO) {
-				mockJwt.EXPECT().ParseToken(dto.Token, true).Return(&payload, nil)
-				mockJwt.EXPECT().VerifyToken(ctx, &payload, true).Return(jwt.ErrNotFound)
-			},
-			expect: func(t *testing.T, err error) {
-				assert.NotNil(t, err)
-				assert.EqualError(t, err, jwt.ErrNotFound.Error())
-			},
-		},
-		{
-			name: "should failed to delete tokens",
-			ctx:  context.Background(),
-			dto: &support.LogoutDTO{
-				Token: "token",
-			},
-			setup: func(ctx context.Context, dto *support.LogoutDTO) {
-				mockJwt.EXPECT().ParseToken(dto.Token, true).Return(&payload, nil)
-				mockJwt.EXPECT().VerifyToken(ctx, &payload, true).Return(nil)
-				mockJwt.EXPECT().DeleteTokens(ctx, &payload).Return(jwt.ErrFailedDeleteToken)
-			},
-			expect: func(t *testing.T, err error) {
-				assert.NotNil(t, err)
-				assert.EqualError(t, err, jwt.ErrFailedDeleteToken.Error())
-			},
-		},
-	}
-
-	for _, tc := range tests {
-		t.Run(tc.name, func(t *testing.T) {
-			tc.setup(tc.ctx, tc.dto)
-			err := service.Logout(tc.ctx, tc.dto)
-			tc.expect(t, err)
 		})
 	}
 }
